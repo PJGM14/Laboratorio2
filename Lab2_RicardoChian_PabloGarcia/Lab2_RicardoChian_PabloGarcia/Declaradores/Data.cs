77c82aefc9c3800f4400df63c099111bd55525d4
Ricardo Chian <ricardochianc@gmail.com>
2019-03-14 02:26:50 -0600

    Declaradores creados

---
+++ Lab2_RicardoChian_PabloGarcia/Lab2_RicardoChian_PabloGarcia/Declaradores/Data.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Estructuras.NoLinearStructures.Trees;
+using Lab2_RicardoChian_PabloGarcia.Models;
+
+namespace Lab2_RicardoChian_PabloGarcia.Declaradores
+{
+    public class Data
+    {
+        private static Data _instance = null;
+
+        public static Data Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    _instance = new Data();
+                }
+                return _instance;
+            }
+        }
+
+        public List<Medicina> ListaMedicinas = new List<Medicina>();
+        public List<Medicina> ListaPedido = new List<Medicina>();
+        public ArbolBinario<Indice> ArbolIndice = new ArbolBinario<Indice>();
+    }
+}
