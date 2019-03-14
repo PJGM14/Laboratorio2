cceb85fb6ac0613d71d16cfd7343fb121c07e4cd
Ricardo Chian <ricardochianc@gmail.com>
2019-03-14 02:22:55 -0600

    Eliminacion de vistas y controladores que vienen por default

--- Lab2_RicardoChian_PabloGarcia/Lab2_RicardoChian_PabloGarcia/Controllers/HomeController.cs
+++ Lab2_RicardoChian_PabloGarcia/Lab2_RicardoChian_PabloGarcia/Controllers/HomeController.cs
@@ -12,19 +12,5 @@ namespace Lab2_RicardoChian_PabloGarcia.Controllers
         {
             return View();
         }
-
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
     }
 }
